50213f052b67d70fc24d52bddb8af98801c7fdf1
QUERY-GANI <ahmadxgani01@gmail.com>
2021-04-16 08:09:55 +0700

    mode debug

--- server/fitures/tiktok_download.js
+++ server/fitures/tiktok_download.js
@@ -1,56 +1,67 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const qs = require('qs')
+const fd = require('form-data')
 
-module.exports = function TIKTOD(url) {
+module.exports = function (url) {
      return new Promise((resolve, reject) => {
-          var BASEurl = 'https://ssstik.io'
+          const activateCookie = 'https://musicallydown.com/?ref=more'
+          const sendReq = 'https://musicallydown.com/download'
           axios.request({
-               url: BASEurl,
+               url: activateCookie,
                method: 'get',
                headers: {
-                    'cookie': process.env.COOKIE_TIKTOK,
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-                    'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"'
+                  'Host': 'musicallydown.com',
+                  'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
+                  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                  'Accept-Language': 'en-US,en;q=0.5',
+                  'Connection': 'keep-alive',
+                  'Cookie': process.env.COOKIE_TIKTOK.slice(0, process.env.COOKIE_TIKTOK.length - 13),
+                  'Upgrade-Insecure-Requests': '1'
                }
           })
           .then(({ data }) => {
-              const $ = cheerio.load(data)
-              const urlPost = $('form[data-hx-target="#target"]').attr('data-hx-post')
-              const tokenJSON = $('form[data-hx-target="#target"]').attr('include-vals')
-              const tt = tokenJSON.replace(/'/g, '').replace('tt:', '').split(',')[0]
-              const ts = tokenJSON.split('ts:')[1]
-              var config = {
-                    headers: {
-                        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                        'cookie': process.env.COOKIE_TIKTOK,
-                        'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-                    },
-                    dataPost: {
-                        'id': url,
-                        'locale': 'en',
-                        'tt': tt,
-                        'ts': ts
-                    }
-              }
-              axios.post(BASEurl + urlPost, qs.stringify(config.dataPost), { headers: config.headers })
-                    .then((results) => {
-                        data = results.data
-                        const $ = cheerio.load(data)
-                        const result = {
-                              caption: $('div > p.maintext').text(),
-                              thumb: $('div > img').attr('src'),
-                              video: $('div > a.without_watermark_direct').attr('href'),
-                              music: $('div > a.music').attr('href')
-                        }
-                        if ($('div > a.without_watermark_direct').attr('href') !== undefined) {
-                              resolve(result)
-                        } else {
-                              reject({ message: 'Tautan ini telah terunduh sebelumnya' })
-                        }
-                    })
-                    .catch(reject)
+            const $ = cheerio.load(data)
+            const link = $('form > div > div > input').eq(0).attr('name')
+            const token = $('form > div > div > input').eq(1).attr('name')
+            const tokenVal = $('form > div > div > input').eq(1).attr('value')
+            
+            const formData = new fd();
+            formData.append(link, url);
+            formData.append(token, tokenVal);
+            formData.append('verify', '1');
+
+            const config = {
+            method: 'post',
+            url: 'https://musicallydown.com/download',
+            headers: { 
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0', 
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 
+            'Accept-Language': 'en-US,en;q=0.5', 
+            'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`, 
+            'Origin': 'https://musicallydown.com', 
+            'Connection': 'keep-alive', 
+            'Referer': 'https://musicallydown.com/?ref=more', 
+            'Cookie': process.env.COOKIE_TIKTOK, 
+            'Upgrade-Insecure-Requests': '1', 
+            'TE': 'Trailers', 
+            },
+            data: formData
+            };
+
+            return axios(config)
+            .then(function (response) {
+                  const jq = cheerio.load(response.data)
+                  const mp4 = jq('div[class="col s12 l8 left-align"] > a').eq(1).attr('href')
+                  const thumb = jq('div[class="col s12 l4 center-align"] > video').attr('poster')
+                  resolve({
+                        mp4,
+                        thumb,
+                  })
+            })
+            .catch(function (error) {
+                  console.log(error);
+            });
+
           })
           .catch(reject)
      })
